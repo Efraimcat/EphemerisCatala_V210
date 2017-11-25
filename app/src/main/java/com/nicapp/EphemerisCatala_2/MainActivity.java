07afb98981877540a96f9542a15d428d8ce9911a
Efraimcat <efraim@efraim.cat>
2017-11-25 17:43:45 +0100

    25 de novembre

--- app/src/main/java/com/nicapp/EphemerisCatala_2/MainActivity.java
+++ app/src/main/java/com/nicapp/EphemerisCatala_2/MainActivity.java
@@ -2,12 +2,11 @@ package com.nicapp.EphemerisCatala_2;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.view.MenuItemCompat;
+//import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,11 +15,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ShareActionProvider;
-
+import android.widget.ProgressBar;
+//import android.widget.ShareActionProvider;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
@@ -30,32 +31,28 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     private WebView mWebView;
     private AdView mAdView;
     private InterstitialAd mInterstitialAd;
     private FirebaseAnalytics mFirebaseAnalytics;
-
+    ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        bar=(ProgressBar) findViewById(R.id.progressBar2);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-//  Firebase Analytics
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-//  Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+//  ADS
+//
         MobileAds.initialize(this, "ca-app-pub-8256835306478664~9754224490");
 //  AdMob Banner
         mAdView = findViewById(R.id.adView);
@@ -65,45 +62,48 @@ public class MainActivity extends AppCompatActivity
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId("ca-app-pub-8256835306478664/1457642512");
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
-//  Prepare an Interstitial Ad Listener
         mInterstitialAd.setAdListener(new AdListener() {
             public void onAdLoaded() {
-//  Call displayInterstitial() function
                 displayInterstitial();
             }
         });
-// Enable Javascript
+//  WEBVIEW
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-//  WebView
         mWebView.loadUrl("https://ephemeris.cat");
-//  Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
-//
+        mWebView.setWebViewClient(new myWebclient());
+//  FIREBASE
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "MY_ITEM_ID-Catala");
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "MY_ITEM_NAME-Catala");
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "MY_CONTENT_TYPE-Catala");
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
-
+//  END onCreate
+//
     public void displayInterstitial() {
-//  If Ads are loaded, show Interstitial else show nothing.
         if (mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         }
     }
-
     @Override
     public void onBackPressed() {
 // WebView
         if(mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
+//      Exit
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            mInterstitialAd.setAdListener(new AdListener() {
+                public void onAdLoaded() {
+                    displayInterstitial();
+                }
+            });
             super.onBackPressed();
         }
     }
-
 //  ACTION BAR
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,51 +111,47 @@ public class MainActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
 //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             alertOneButton();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
 //  MENU
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_manage) {
-            // Handle
-        } else if (id == R.id.nav_share) {
-
+//      Compartir
+        if (id == R.id.nav_share) {
             Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
             sharingIntent.setType("text/plain");
-            String shareBodyText = "https://play.google.com/store/apps/details?id=com.nicapp.EphemerisCatala_2";
+            String shareBodyText = getString(R.string.url_app);
             sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,"Ephemeris Català");
             sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBodyText);
             startActivity(Intent.createChooser(sharingIntent, "Shearing Option"));
-
         } else if (id == R.id.nav_send) {
-
+//      Salir App
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            mInterstitialAd.setAdListener(new AdListener() {
+                public void onAdLoaded() {
+                    displayInterstitial();
+                }
+            });
+//
             ActivityCompat.finishAffinity(this);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 //  Mensaje cabecera
     public void alertOneButton() {
         new AlertDialog.Builder(MainActivity.this)
@@ -163,9 +159,39 @@ public class MainActivity extends AppCompatActivity
                 .setMessage(R.string.Missatge)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
                         dialog.cancel();
                     }
            }).show();
     }
+//  WEBVIEW
+    public class myWebclient extends WebViewClient{
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            bar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode==KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
 }
